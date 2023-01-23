24534918eed7cb2659a73713fce872b15dd3846b
KyleMcMullinUSU <kyle.mcmullin@usu.edu>
2023-01-23 14:36:11 -0700

    fixing styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 interface Quote {
@@ -9,38 +8,49 @@ interface Quote {
 
 function App() {
   const [searchInput, setSearchInput] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState<Quote[] | null>([]);
   const [randomQuote, setRandomQuote] = useState<Quote | null>(null);
+  const [headerDisplay, setHeaderDisplay] = useState("home-header");
+  const [searchDisplay, setSearchDisplay] = useState("home-search");
+  const [visible, setVisible] = useState("visible");
 
   async function findRandom() {
     const result = await fetch("https://usu-quotes-mimic.vercel.app/api/random");
     setRandomQuote(await result.json());
   }
 
+  async function performSearch() {
+    const results = await fetch(`https://usu-quotes-mimic.vercel.app/api/search?query=${searchInput}`);
+    setSearchResults(await results.json());
+  }
+
   useEffect(() => {
     findRandom();
   }, []);
 
-  function performSearch() {
-    fetch(`https://usu-quotes-mimic.vercel.app/api/search?query=${searchInput}`)
-      .then(res => res.json())
-      .then((results) => {
-        setSearchResults(results.results)
-      });
+  const handleEnter = (event: { key: String }) => {
+    if (event.key == "Enter") {
+      performSearch();
+      setHeaderDisplay("results-header");
+      setSearchDisplay("results-search");
+      setSearchInput("");
+      setVisible("hidden");
+    }
   }
 
   return (
     <div className="App">
-      <h1>Quote Search</h1>
-      <div className="card">
+      <h1 id={headerDisplay}>Quote Search</h1>
+      <div className={searchDisplay}>
         <input
           className = "round"
           type="search"
           placeholder="Search for a quote"
           value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)} />
+          onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown = {handleEnter} />
       </div>
-      <div className="random-quote">
+      <div className="random-quote" id={visible}>
         <p>{(randomQuote === null) ? "" : randomQuote.content}</p>
         <h4>-{(randomQuote !== null) ? ((randomQuote.author !== "")  ? randomQuote.author : "Unknown") : ""}</h4>
       </div>
