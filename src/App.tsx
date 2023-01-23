8927b3cc37205a6d451dd65bf13757f2eb091ba9
KyleMcMullin <mcmullinjkyle@gmail.com>
2023-01-23 10:58:42 -0700

    Random basically done

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [randomQuote, setRandomQuote] = useState("");
+  const [randomAuthor, setRandomAuthor] = useState("");
+
+  useEffect(() => {
+    fetch("https://usu-quotes-mimic.vercel.app/api/random")
+      .then(res => res.json())
+      .then((quote) => {
+        setRandomQuote(quote.content)
+        setRandomAuthor(quote.author)
+      })
+    return () => {}
+  }, []);
+
+  function performSearch() {
+    console.log('searching for', searchInput);
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Quote Search</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <input
+          type="search"
+          placeholder="Search for a quote"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="random-quote">
+        <p>{randomQuote}</p>
+        <h4>-{randomAuthor}</h4>
+      </div>
     </div>
   )
 }
